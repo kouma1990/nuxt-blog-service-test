9ef9e7b286068fd6858ac97d2e62a08352d1ca0c
kouma1990 <kouma1990@gmail.com>
2019-02-13 00:56:41 +0900

    add: トップページと一覧ページ仮

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="container posts-page">
+        <el-card>
+            <div slot="header" class="clearfix">
+                <span>新規登録</span>
+            </div>
+            <el-table :data="showPosts" style="width: 100%" class="table">
+                <el-table-column prop="title" label="タイトル"></el-table-column>
+                <el-table-column prop="user.id" label="投稿者" width="180"></el-table-column>
+                <el-table-column prop="created_at" label="投稿日時" width="240"></el-table-column>
+            </el-table>
+        </el-card>
+
+    </section>
+
+</template>
+
+<script>
+export default {
+    computed: {
+        showPosts() {
+            return [
+                {
+                    id: "001",
+                    title: "title1",
+                    body: "body1",
+                    created_at: "2019/2/13 12:00:00",
+                    user: {
+                        id: "potato4d"
+                    }
+                },
+                {
+                    id: "002",
+                    title: "title2",
+                    body: "body2",
+                    created_at: "2019/2/13 13:00:00",
+                    user: {
+                        id: "potato4d"
+                    }
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+    cursor: pointer;
+}
+</style>
